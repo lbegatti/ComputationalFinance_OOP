2843c445f492ff2261117e9a94b94437bd51dc0c
Luca Begatti <luca.begatti93@gmail.com>
2022-11-24 22:51:28 +0100

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Luca Begatti on 24/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
